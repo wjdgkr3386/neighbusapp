720db746fe69342e4956eb3fb5dd933e0ee06b9b
jeonghak <wjdgkr3386@gmail.com>
2025-12-19 10:50:24 +0900

    로그인 기능

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 // src/config.js
 
-// 1. 로컬 개발용 (지금 이거 쓰세요)
-export const BASE_URL = 'http://10.0.2.2:8090';
+// 로컬 개발용 (가상환경)
+//export const BASE_URL = 'http://10.0.2.2:8080';
 
-// 2. 실제 서버용 (나중에 주석 풀고 이거 쓰세요)
-// export const BASE_URL = 'http://xn--2e0bn4enxcyt6b.xn--yq5b.xn--3e0b707e:8080';
+// 로컬 개발용 (실제폰)
+export const BASE_URL = 'http://10.155.174.62:8080';
+
+// 실제 서버용
+// export const BASE_URL = 'http://43.200.155.122:8080';
+
+
+
